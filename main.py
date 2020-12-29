d8a583036fd78f6c4c73286195372bb0b88b4389
mrghosti3 <mysteriousghst@gmail.com>
2020-12-29 02:25:28 +0200

    Added cmd arguments parsing

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-import argparse
+from argparse import ArgumentParser
 import feedparser
 
 from articledatabase import articleDatabase as articleDB
+
+def create_argparse() -> ArgumentParser:
+    """Generates argument parser
+
+    Returns:
+        ArgumentParser: Object to parse command line arguments
+    """
+
+    arguementParser = ArgumentParser(description='Reads RSS feeds and stores articles to sql format database with file extension ".adb"')
+    arguementParser.add_argument('input_type', choices=['FILE', 'URL'],
+        help=''
+    )
+    arguementParser.add_argument("input", help='')
+    arguementParser.add_argument('-o', '--output',
+        metavar='PATH', default='./articles.adb',
+        help='Location and name of output file. Default: ./articles.adb'
+    )
+    arguementParser.add_argument('-v', '--verbose', action='store_true',
+        help='Verbose logging mode'
+    )
+    return arguementParser
 
 def read_article_feed(url, articles):
     """Reads article feed from single url
@@ -17,9 +38,7 @@ def read_article_feed(url, articles):
             articles.add_article_to_db(article['title'], article['published'])
 
 if __name__ == '__main__':
-    print('running')
+    argParser = create_argparse()
+    args = argParser.parse_args()
 
-    articles = articleDB('output')
-
-    read_article_feed("", articles)
-    articles.close_database()
+    articles = articleDB(args.output)
